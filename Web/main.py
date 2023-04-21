183cb136452310d9510b04069cd26a31f24955ff
bluebluebluesky <1684604058@qq.com>
2023-04-21 21:04:02 +0800

    main.py

--- Web/main.py
+++ Web/main.py
@@ -4,21 +4,24 @@ from flask import Flask, render_template, request
 from utils import GPUs
 from Model import OpenAssistant
 
-
-GPUs.init_gpu()
+#GPUs.init_gpu()
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('new.html')
 
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    inputs = request.form['dialog']
-    dialog = OpenAssistant.forward(inputs, max_new_tokens=150)
-    end = "<|endoftext|>" in dialog[len(inputs):]
-    reply = dialog[len(inputs):].replace("<|endoftext|>", "")
-    return {"reply": reply, "dialog": dialog, "ts": request.form['ts'], 'end': end}
+@app.route('/feature')
+def feature():
+    return render_template('feature.html')
+
+
+#@app.route('/chat', methods=['POST'])
+#def chat():
+#    inputs = request.form['dialog']
+#    dialog = OpenAssistant.forward(inputs, max_new_tokens=150)
+#    end = "<|endoftext|>" in dialog[len(inputs):]
+#    reply = dialog[len(inputs):].replace("<|endoftext|>", "")
+#    return {"reply": reply, "dialog": dialog, "ts": request.form['ts'], 'end': end}
