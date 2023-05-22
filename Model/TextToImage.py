09c53d319b86483b16b4efc4d93aacd657261f72
silvia925 <ziruiwang@smail.nju.edu.cn>
2023-05-22 19:27:07 +0800

    The author has unified the interfaces of some open-source models to facilitate the main model's invocation.

---
+++ Model/TextToImage.py
@@ -0,0 +1,17 @@
+from diffusers import StableDiffusionPipeline
+import torch
+
+def stable_diffusion_v1_5(prompt, output_slot_id):
+    # 输入字符串
+    model_dir = "cache/model"
+
+    model_id = "runwayml/stable-diffusion-v1-5"
+    pipe = StableDiffusionPipeline.from_pretrained(model_id)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    pipe.to(device)
+
+    image = pipe(prompt).images[0]  
+        
+    image.save(f"./cache/fileslots/{output_slot_id}.png")
+
+    return image
