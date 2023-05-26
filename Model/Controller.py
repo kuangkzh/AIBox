9136a48a5df99bf5b596ebb384e3c2d9b31e08fd
yumy <yumy_1119@163.com>
2023-05-26 21:28:23 +0800

    update Controller

--- Model/Controller.py
+++ Model/Controller.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, AutoModel, AutoProcessor
 import re
-import ImageCaptioning, Browser, SpeechRecognition, TextToImage, TextToVideo, VisualQuestionAnswering
+import ImageCaptioning, SpeechRecognition, TextToImage, TextToVideo, VisualQuestionAnswering
 
 class Controller:
     def __call__(self, input_str):
@@ -10,13 +10,15 @@ class Controller:
         if self.model_name == "vit-gpt2-image-captioning":
             return(ImageCaptioning.vit_gpt2_image_captioning(self.model_args))
         if self.model_name == "whisper-base":
-            return(SpeechRecognition.whisper_base(self.model_args))
+            return(SpeechRecognition.whisper_base(*self.model_args))
         if self.model_name == "stable-diffusion-v1-5":
-            return(TextToImage.stable_diffusion_v1_5(self.model_args))
+            return(TextToImage.stable_diffusion_v1_5(*self.model_args))
         if self.model_name == "text-to-video-ms-1.7b":
-            return(TextToVideo.text_to_video(self.model_args))
+            return(TextToVideo.text_to_video(*self.model_args))
         if self.model_name == "vilt-b32-finetuned-vqa":
-            return(VisualQuestionAnswering.visual_question_answer(self.model_args))
+            return(VisualQuestionAnswering.visual_question_answer(*self.model_args))
+        if self.model_name == "python":
+            return(eval(self.model_args[0]))
         
         # return("model name: {}; model args: {}.".format(self.model_name, self.model_args))
   
@@ -29,7 +31,7 @@ class Controller:
         if start_pos < 0: 
             return -1
         start_pos += 8
-        end_pos = input_str.find('<|endofcall|>')
+        end_pos = input_str.find('<|end|>')
         if end_pos < 0:
             return -1
         call_str = input_str[start_pos:end_pos]
@@ -48,11 +50,15 @@ class Controller:
         end_pos = call_str.find(')')
         if end_pos < 0:
             return -1
-        self.model_args = eval(call_str[start_pos+1: end_pos])
+        args = eval(call_str[start_pos+1: end_pos])
+        if (type(args) == int or type(args) == str):
+            self.model_args = [args]
+        else:
+            self.model_args = list(args)
         return 1
         
 
   
 if __name__ == "__main__":
     controller = Controller() 
-    print(controller("<|call|>[vit-gpt2-image-captioning](1, 2)<|endofargs|>results<|endofcall|>"))
+    print(controller("<|call|>[vilt-b32-finetuned-vqa](\"What is the color of the bed?\",10)<|end|>"))
